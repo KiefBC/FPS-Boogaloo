c183b7ad4ddbc8952dcea089209eae7ac29ec5bb
Kiefer <kiefhay642@outlook.com>
2025-02-06 19:37:02 -0800

    Lab 4 done

--- Assets/Scripts/FPSInput.cs
+++ Assets/Scripts/FPSInput.cs
@@ -3,10 +3,12 @@ using UnityEngine;
 public class FPSInput : MonoBehaviour
 {
     public float speed = 5.0f;
+    [SerializeField] private CharacterController charController;
+    private float gravity = -9.8f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -17,8 +19,9 @@ public class FPSInput : MonoBehaviour
         float deltaZ = Input.GetAxis("Vertical");
         Vector3 movement = new Vector3(deltaX, 0, deltaZ);
         movement = Vector3.ClampMagnitude(movement, 1);
+        movement.y = gravity;
         movement *= Time.deltaTime * speed;
         movement = transform.TransformDirection(movement);
-        GetComponent<CharacterController>().Move(movement);
+        charController.Move(movement);
     }
 }
